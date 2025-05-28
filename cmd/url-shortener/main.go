9f91e34e910fa48508c5846b227e8b0fffe5deb3
DaniilKalts <kaltsdaniil@gmail.com>
2025-05-28 17:13:10 +0500

    chore: add TODOs in main.go

---
+++ cmd/url-shortener/main.go
@@ -0,0 +1,13 @@
+package main
+
+func main() {
+	// TO-DO: init configuration file
+
+	// TO-DO: init logger (slog)
+
+	// TO-DO: init storage (sqlite)
+
+	// TO-DO: init router (chi)
+
+	// TO-DO: run server (http)
+}
